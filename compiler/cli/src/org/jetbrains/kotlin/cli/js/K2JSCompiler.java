ea6b3b1a78127d3f584303fc190a5170c928b1d4
Dmitry Savvinov <dsavvinov@gmail.com>
2018-03-29 11:37:00 +0300

    Rewrite performance statistics collection
    This commit introduces notion of 'PerformanceManager' in CLI, suitable
    for collecting performance metrics of the compiler. It:
    
    - provides `notifyX{Started/Finished}` API, where 'X' is some
    measurable event (previously there were just ad hoc manual time
    measurements using System.nanoTime() and stuff)
    
    - collects measurements, so that later they can be reported in an
    appropriate way (previously measurements were reported immediately to
    MessageCollector as plain strings)
    
    - allows overriding to collect metrics, specific for just one target
    platform compilation
    
    Also, common logic of compiler performance statistics collection was
    extracted from platform-compilers (K2JVMCompiler) to common classes
    (CLICompiler), to allow other platform-compilers (e.g. K2JSCompiler)
    re-use it.

--- compiler/cli/src/org/jetbrains/kotlin/cli/js/K2JSCompiler.java
+++ compiler/cli/src/org/jetbrains/kotlin/cli/js/K2JSCompiler.java
@@ -33,6 +33,7 @@ import org.jetbrains.kotlin.analyzer.AnalysisResult;
 import org.jetbrains.kotlin.backend.common.output.OutputFileCollection;
 import org.jetbrains.kotlin.cli.common.CLICompiler;
 import org.jetbrains.kotlin.cli.common.CLIConfigurationKeys;
+import org.jetbrains.kotlin.cli.common.CommonCompilerPerformanceManager;
 import org.jetbrains.kotlin.cli.common.ExitCode;
 import org.jetbrains.kotlin.cli.common.arguments.K2JSCompilerArguments;
 import org.jetbrains.kotlin.cli.common.arguments.K2JsArgumentConstants;
@@ -94,6 +95,8 @@ public class K2JSCompiler extends CLICompiler<K2JSCompilerArguments> {
     public static void main(String... args) {
         doMain(new K2JSCompiler(), args);
     }
+
+    private final K2JSCompilerPerformanceManager performanceManager = new K2JSCompilerPerformanceManager();
 
     @NotNull
     @Override
@@ -511,6 +514,11 @@ public class K2JSCompiler extends CLICompiler<K2JSCompilerArguments> {
         return commonPath != null ? commonPath.getPath() : ".";
     }
 
+    @NotNull
+    @Override
+    protected CommonCompilerPerformanceManager getPerformanceManager() {
+        return performanceManager;
+    }
 
     private static MainCallParameters createMainCallParameters(String main) {
         if (K2JsArgumentConstants.NO_CALL.equals(main)) {
@@ -526,4 +534,12 @@ public class K2JSCompiler extends CLICompiler<K2JSCompilerArguments> {
     public String executableScriptFileName() {
         return "kotlinc-js";
     }
+
+    private static final class K2JSCompilerPerformanceManager extends CommonCompilerPerformanceManager {
+        public K2JSCompilerPerformanceManager() {
+            super("Kotlin to JS Compiler");
+        }
+    }
+
+
 }
